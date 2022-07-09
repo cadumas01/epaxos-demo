7b2c04f535cd8ab35960d17276633349be7aed8d
cadumas01 <cadumas01@gmail.com>
2022-07-08 21:08:01 -0400

    fix test.sh?

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 timeout 60s bin/client -maddr=10.182.0.17 -writes=$1 -c=$2 -T=$3
 tail -n +200 latFileRead-0.txt  > tmp.txt && mv tmp.txt latFileRead-0.txt
 tail -n +200 latFileRead-1.txt  > tmp.txt && mv tmp.txt latFileRead-1.txt
@@ -5,5 +7,5 @@ tail -n +200 latFileRead-2.txt  > tmp.txt && mv tmp.txt latFileRead-2.txt
 tail -n +200 latFileWrite-0.txt  > tmp.txt && mv tmp.txt latFileWrite-0.txt
 tail -n +200 latFileWrite-1.txt  > tmp.txt && mv tmp.txt latFileWrite-1.txt
 tail -n +200 latFileWrite-2.txt  > tmp.txt && mv tmp.txt latFileWrite-2.txt
-python3 client_metrics.py
+# python3 client_metrics.py
 
