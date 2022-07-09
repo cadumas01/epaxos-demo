377d66ae46e5409c3dcf74e7cb69fd3b9238f687
cadumas01 <cadumas01@gmail.com>
2022-07-09 19:28:55 -0400

    fix indent

--- set_delays.py
+++ set_delays.py
@@ -4,12 +4,13 @@ def usage():
     print("Usage: python3 set_delays.py NODE_#")
 
 if __name__ == "__main__":
+    file_path = "ips.json"
     file = open(file_path, "r+")
     ips = json.load(file)    
 
     if len(sys.args) != 2 :
         usage()
-        
+
     command = "sudo tc qdisc del dev ens4 root; sudo tc qdisc add dev ens4 root handle 1: htb; sudo tc class add dev ens4 parent 1: classid 1:1 htb rate 1gibps; sudo tc class add dev ens4 parent 1:1 classid 1:2 htb rate 1gibps; sudo tc qdisc add dev ens4 handle 2: parent 1:2 netem delay {}ms sudo tc filter add dev  ens4 pref 2 protocol ip u32 match ip dst {} flowid 1:2; sudo tc class add dev ens4 parent 1: classid 1:1 htb rate 1gibps; sudo tc class add dev ens4 parent 1:1 classid 1:3 htb rate 1gibps; sudo tc qdisc add dev ens4 handle 3: parent 1:3 netem delay {}ms sudo tc filter add dev  ens4 pref 3 protocol ip u32 match ip dst {} flowid 1:3;"    
     if sys.argv[1] == 0:
         os.system(command.format(ips["1"], "36", ips["2"], "75.5"))
--- set_ips.py
+++ set_ips.py
@@ -7,26 +7,13 @@ from textwrap import indent
 # run via:
 # python update_config <config_num> <protocol>
 
-# updates a json file
-def update(file_path, key, new_value):
-    file = open(file_path, "r+")
-    json_object = json.load(file)
-    
-
-    json_object[key] = new_value
-
-    file = open(file_path, "w")
-    json.dump(json_object, file, indent=4)
-    # file.write(simplej)
-
-    file.close()
-
 
 print("Called set_ips.py")
 if len(sys.argv) != 4:
     print("Usage: python3 set_ips <INTERNAL_IP_CALIFORNIA> <INTERNAL_IP_VIRGINIA> <INTERNAL_IP_IRELAND>")
     exit(1)
 
+file_path = "ips.json"
 file = open(file_path, "r+")
 json_object = json.load(file)
 file.close()
@@ -37,7 +24,6 @@ for i in range(0,3):
     json_object[server[i]] = sys.argv[i + 1]
     print("Just set " , server[i])
 
-file_path = "ips.json"
 file = open(file_path, "w")
 json.dump(json_object, file, indent=4)
 
