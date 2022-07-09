0c400deb48b936cc2850d7258e2843c80fd06b70
cadumas01 <cadumas01@gmail.com>
2022-07-09 19:49:33 -0400

    launch epaxos

---
+++ launch_epaxos.py
@@ -0,0 +1,24 @@
+import sys
+import json
+import os
+
+def usage():
+    print("Usage: python3 launch_epaxos.py NODE_#")
+
+if __name__ == "__main__":
+    file_path = "ips.json"
+    file = open(file_path, "r+")
+    ips = json.load(file)    
+
+    if len(sys.argv) != 2 :
+        usage()
+
+    command = "bin/master -N 3 & bin/server -maddr={} -addr={} -gus=false -e=true -exec=true -dreply=true &"
+        os.system(command.format( ips["0"], ips["0"] ))
+    elif  sys.argv[1] == "1":
+        os.system(command.format( ips["0"], ips["1"] ))
+    elif sys.argv[1] == "2":
+        os.system(command.format( ips["0"], ips["2"] ))
+    else:
+        print("Invalid NODE #")
+        usage()
