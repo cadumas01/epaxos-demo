0b7a5425ff924e74f80824610f88325db9c23204
cadumas01 <cadumas01@gmail.com>
2022-07-10 09:56:38 -0400

    remove prints in set ips

--- set_ips.py
+++ set_ips.py
@@ -7,8 +7,6 @@ from textwrap import indent
 # run via:
 # python update_config <config_num> <protocol>
 
-
-print("Called set_ips.py")
 if len(sys.argv) != 4:
     print("Usage: python3 set_ips <INTERNAL_IP_CALIFORNIA> <INTERNAL_IP_VIRGINIA> <INTERNAL_IP_IRELAND>")
     exit(1)
@@ -22,7 +20,6 @@ servers = ["0", "1", "2"]
 
 for i in range(0,3):
     json_object[servers[i]] = sys.argv[i + 1]
-    print("Just set " , servers[i])
 
 file = open(file_path, "w")
 json.dump(json_object, file, indent=4)
